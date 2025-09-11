99c0406b901f66a83c8138668a2338bdcee3d913
alaczkirito <cao.alacz@gmail.com>
2025-09-11 17:24:19 +0800

    Database files created

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+﻿<?php
+$db = "mysql:host=localhost;dbname=vouchercartsystem;charset=utf8";
+$dbuser = "root";
+$dbpasswd = "";
+
+try {
+    $pdo = new PDO($db, $dbuser, $dbpasswd);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
---
+++ formhandler.php
@@ -0,0 +1,16 @@
+﻿<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$username = $_POST['fullName'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+try {
+require_once 'dbconnect.php';
+} catch (PDOException $e) {
+    die("Query failed: " . $e->getMessage());
+}
+}
+else
+{
+    header("location:index.html");
+}
